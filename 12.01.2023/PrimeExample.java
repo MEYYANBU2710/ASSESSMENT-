f74950759780b49a7e6fd6780d60e3c618fb304d
MEYYANBU2710 <119842693+MEYYANBU2710@users.noreply.github.com>
2023-01-12 15:41:29 +0530

    Add files via upload

---
+++ 12.01.2023/PrimeExample.java
@@ -0,0 +1,16 @@
+
+class prime{
+		public static void main(String... args)
+		{
+			int a=1, b=0, n=17;
+			
+		for (int i=1; i<=n; i++){
+			if(n/i==n && n/i==0)
+			{
+				System.out.println("123");
+			}
+		}
+		}
+}
+			
+			
